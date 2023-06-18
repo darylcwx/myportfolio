0a73ad17a0e9159afc02ddf806ff47f7db30f653
daryl <darylchua@hotmail.sg>
2023-06-19 01:39:47 +0800

    animation styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,6 +43,51 @@ export default function Navbar({ location }) {
 			color: theme.palette.secondary.main,
 		},
 	};
+	const sideNav = {
+		open: (height = 1000) => ({
+			clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+			transition: {
+				type: "spring",
+				stiffness: 20,
+				restDelta: 2,
+			},
+		}),
+		closed: {
+			clipPath: "circle(30px at 40px 40px)",
+			transition: {
+				delay: 0.5,
+				type: "spring",
+				stiffness: 400,
+				damping: 40,
+			},
+		},
+	};
+	const ul = {
+		open: { transition: { staggerChildren: 0.07, delayChildren: 0.2 } },
+		closed: {
+			transition: {
+				staggerChildren: 0.1,
+				staggerDirection: -1,
+			},
+		},
+	};
+	const li = {
+		open: {
+			y: 0,
+			opacity: 1,
+			transition: {
+				y: { stiffness: 1000 },
+			},
+		},
+		closed: {
+			y: 100,
+			opacity: 0,
+			transition: {
+				y: { stiffness: 1000 },
+			},
+		},
+	};
+
 	useEffect(() => {
 		const handleScroll = () => {
 			setScrollPosition(window.scrollY);
