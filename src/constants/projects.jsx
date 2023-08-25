e81ca9e80d832f719e8ca91865368ffd0277db2f
daryl <darylchua@hotmail.sg>
2023-08-25 18:02:17 +0800

    added mongoDB

---
+++ src/constants/projects.jsx
@@ -0,0 +1,241 @@
+const projects = [
+	{
+		title: "This very website!",
+		src: "myportfolio.png",
+		frameworks: [
+			{
+				name: "React",
+				link: "https://reactjs.org/",
+				src: "react.png",
+			},
+			{
+				name: "Material UI",
+				link: "https://chakra-ui.com/",
+				src: "chakraui.jpg",
+			},
+			{
+				name: "Framer Motion",
+				link: "https://www.framer.com/motion/",
+				src: "framermotion.png",
+			},
+			{
+				name: "Telegram Bot API",
+				link: "https://core.telegram.org/",
+				src: "telegram.png",
+			},
+		],
+		platforms: [
+			{
+				name: "Vercel App",
+				link: "https://darylchua.vercel.app/",
+				src: "vercel2.png",
+			},
+			{
+				name: "Github",
+				link: "https://github.com/darylcwx/myportfolio",
+				src: "github.png",
+			},
+		],
+		description: `I've always admired how artists in their respective fields showcase their work through their Instagram/website portfolios. 
+        As a passionate programmer with a keen interest in design, I couldn't resist the opportunity to create my own portfolio website. 
+        I truly hope that this application will always be a WIP, because it simply means that I never stop learning and growing!`,
+	},
+	{
+		title: "Seiko Mods Artisan",
+		src: "smodsartisan.png",
+		frameworks: [
+			{
+				name: "Next.js",
+				link: "https://nextjs.org/",
+				src: "nextjs.png",
+			},
+			{
+				name: "Mantine UI",
+				link: "https://mantine.dev/",
+				src: "mantine.png",
+			},
+			{
+				name: "Tailwind CSS",
+				link: "https://tailwindcss.com/",
+				src: "tailwind.png",
+			},
+			{
+				name: "MongoDB",
+				link: "https://www.mongodb.com/",
+				src: "mongodb.png",
+			},
+			{
+				name: "Framer Motion",
+				link: "https://www.framer.com/motion/?gad=1",
+				src: "framermotion.png",
+			},
+			{
+				name: "Telegram Bot API",
+				link: "https://core.telegram.org/",
+				src: "telegram.png",
+			},
+		],
+		platforms: [
+			{
+				name: "Vercel App",
+				link: "https://smods-artisan.vercel.app/",
+				src: "vercel2.png",
+			},
+			{
+				name: "Github",
+				link: "https://github.com/darylcwx/smods-artisan",
+				src: "github.png",
+			},
+		],
+		description: `A landing page designed for the watches I have custom made so far, Seiko Mods Artisan is a website I'm exploring to implement a small online store, 
+        which allows me to explore all about React and various APIs that could help support my application. WIP.
+        <br><br><i>You make watches?</i>
+        <br>Yes I do. Click the picture above to find out more! 🤩`,
+	},
+	{
+		title: "Receipt Calculator",
+		src: "receiptcalculator.png",
+		frameworks: [
+			{
+				name: "Bootstrap",
+				link: "https://getbootstrap.com/",
+				src: "bootstrap.png",
+			},
+			{
+				name: "Telegram Bot API",
+				link: "https://core.telegram.org/",
+				src: "telegram.png",
+			},
+			{
+				name: "OCR Space API",
+				link: "https://ocr.space/OCRAPI",
+				src: "OCRspace.png",
+			},
+		],
+		platforms: [
+			{
+				name: "Vercel App",
+				link: "https://receipt-calculator.vercel.app/",
+				src: "vercel2.png",
+			},
+			{
+				name: "Github",
+				link: "https://github.com/darylcwx/receiptCalculator",
+				src: "github.png",
+			},
+		],
+
+		description: `A small project inspired by having people in my life who wish to ensure a correct split of a bill, Receipt Calculator endeavours to solve this real-world problem. 
+        By allowing users to input names, food/items, prices, and the participation of each person, the exact share and how much money to transfer for each person can be calculated. 
+        <br><br>Optical Character Recognition through taking a picture of the receipt was implemented recently!
+        WIP: Experimentation with Paylah! API.`,
+	},
+	// {
+	// 	title: "Chess Game",
+	// 	src: "chessgame.png",
+	// 	frameworks: [
+	// 		{
+	// 			name: "Next.js",
+	// 			link: "https://nextjs.org/",
+	// 			src: "nextjs.png",
+	// 		},
+	// 		{
+	// 			name: "Chakra UI",
+	// 			link: "https://chakra-ui.com/",
+	// 			src: "chakraui.jpg",
+	// 		},
+	// 		{
+	// 			name: "Tailwind CSS",
+	// 			link: "https://tailwindcss.com/",
+	// 			src: "tailwind.png",
+	// 		},
+	// 		{
+	// 			name: "Telegram Bot API",
+	// 			link: "https://core.telegram.org/",
+	// 			src: "telegram.png",
+	// 		},
+	// 	],
+	// 	platforms: [
+	// 		// {
+	// 		// 	name: "Vercel App",
+	// 		// 	link: "https://null.vercel.app/",
+	// 		// 	src: "vercel2.png",
+	// 		// },
+	// 		{
+	// 			name: "Github",
+	// 			link: "https://github.com/jadatanjq/chess-game",
+	// 			src: "github.png",
+	// 		},
+	// 	],
+	// 	description: `A microservice architecture based application that runs online chess games for 2 friends.
+	// 	WIP.`,
+	// },
+	{
+		title: "Bukit Better Driving Center",
+		src: "https://www.youtube.com/embed/8TnJ-BvkvGo",
+		// src: "https://youtu.be/8TnJ-BvkvGo",
+		frameworks: [
+			{ name: "Vue.js", link: "https://vuejs.org/", src: "vue.png" },
+			{
+				name: "Quasar",
+				link: "https://quasar.dev/",
+				src: "quasar.png",
+			},
+			{
+				name: "GreenSock",
+				link: "https://greensock.com/gsap/",
+				src: "gsap.png",
+			},
+			{
+				name: "Firebase",
+				link: "https://firebase.google.com/",
+				src: "firebase.png",
+			},
+		],
+		platforms: [
+			{
+				name: "Netlify App",
+				link: "https://bukitbetterdrivingcenter.netlify.app/",
+				src: "netlify.png",
+			},
+			{
+				name: "Github",
+				link: "https://github.com/raphaelgohwx/bbdc-project/",
+				src: "github.png",
+			},
+		],
+		description: `Bukit Better Driving Center is a school project designed to revamp what was inspired by Bukit Batok Driving Center's website. It was my first step to leverage on proper frameworks and databases to build a practical and relevant use case, which later on result in the birth of my own website.`,
+	},
+	{
+		title: "Microsoft Excel Order Tracking System",
+		src: "https://www.youtube.com/embed/czirF7NbzsQ",
+		// src: "https://youtu.be/czirF7NbzsQ",
+		frameworks: [
+			{
+				name: "Google Forms",
+				link: "https://www.google.com/forms/about/",
+				src: "gforms.png",
+			},
+			{
+				name: "Zapier",
+				link: "https://zapier.com/",
+				src: "zapier.png",
+			},
+			{
+				name: "MS Excel + VBA",
+				link: "https://learn.microsoft.com/en-us/office/vba/api/overview/",
+				src: "excel.png",
+			},
+		],
+		platforms: [
+			{
+				name: "Imported to OneDrive",
+				link: "https://1drv.ms/x/s!Aj8bmYh8HCEVwXGK781zbE4haPb1",
+				src: "onedrive.png",
+			},
+		],
+		description: `This Order Tracking system was a side project that I freelanced to build. Employees enter a new order through a Google Form, which Zapier detects upon submission. Order details are then pulled into a OneDrive excel file. 
+        Macros help to generate invoices, receipts, and delivery orders, as well as preparing a ready-to-send templated email to the client.<br><br>Beyond theoretical knowledge, this was my first proper project that physically introduced me into the world of automation.`,
+	},
+];
+export default projects;
--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -5,49 +5,8 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { Parallax } from "react-scroll-parallax";
 import EducationComponent from "../components/EducationComponent";
+import educations from "../constants/education";
 const Education = forwardRef((props, ref) => {
-	const educations = [
-		{
-			id: 2,
-			school: "Singapore Management University",
-			degree: "Bachelor's Degree in Information Systems",
-			tracks: ["Digital Cloud Solutions", "Business Analytics"],
-			awards: [],
-			coCurricular: ["SMU Blockchain Club", "SMU Muay Thai"],
-			period: "2021 to ~2025",
-			gpa: "3.43",
-			image: "./images/education/smu.jpg",
-			first: true,
-		},
-		{
-			id: 1,
-			school: "Ngee Ann Polytechnic",
-			degree: "Diploma in Financial Informatics (School of Infocomm Technology)",
-			tracks: ["Financial Analytics"],
-			awards: [
-				"Director's List for excellent academic performance Academic Year 2017/2018",
-			],
-			coCurricular: [
-				"Member of ICT Society (2017-2018)",
-				"Group Leader for ICT Society Camp (2017)",
-				"Head of Welfare for ICT Welcoming Camp (2018)",
-			],
-			period: "2016 to 2019",
-			gpa: "3.61",
-			image: "./images/education/np.jpg",
-		},
-		{
-			id: 0,
-			school: "Anglo-Chinese School (Barker Road)",
-			degree: "GCE 'O' Levels",
-			tracks: [],
-			awards: [],
-			coCurricular: ["Waterpolo (2013 - 2014)", "Handball (2015 - 2016)"],
-			period: "2012 to 2015",
-			image: "./images/education/acsbr.jpg",
-			last: true,
-		},
-	];
 	return (
 		<Box
 			name="education"
